abb1dbb8c6a8a6753ce15a92465e7d7bf20860d6
Nick Vandewiele <nickvandewiele@gmail.com>
2018-02-23 10:09:33 -0500

    add react
    create a react component that lists the users on the index.html page

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 
 # instantiate db
 db = SQLAlchemy()
@@ -13,6 +14,9 @@ def create_app():
 
 	# instantiate the app
 	app = Flask(__name__)
+
+	# enable CORS
+	CORS(app)
 
 	# set config
 	app_settings = os.getenv('APP_SETTINGS')
