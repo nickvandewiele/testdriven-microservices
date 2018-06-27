df70c5c4d2036aaaf1677346d267a1648a14eade
Nick Vandewiele <nickvandewiele@gmail.com>
2018-06-27 14:34:02 -0400

    /logout route

--- services/users/project/api/auth.py
+++ services/users/project/api/auth.py
@@ -60,7 +60,7 @@ def register_user():
 
 @auth_blueprint.route('/auth/login', methods=['POST'])
 def login_user():
-    
+
     # get post data
     post_data = request.get_json()
     response_object = {
@@ -94,9 +94,28 @@ def login_user():
         return jsonify(response_object), 500
 
 @auth_blueprint.route('/auth/logout', methods=['GET'])
-def user_logout():  
-    pass
+def logout_user():
+    # get auth token
+    auth_header = request.headers.get('Authorization')
+    response_object = {
+        'status': 'fail',
+        'message': 'Provide a valid auth token.'
+    }
 
+    if auth_header:
+        auth_token = auth_header.split(' ')[1]
+        resp = User.decode_auth_token(auth_token)
+
+        if not isinstance(resp, str):
+            response_object['status'] = 'success'
+            response_object['message'] = 'Successfully logged out.'
+            return jsonify(response_object), 200
+        
+        else:
+            response_object['message'] = resp
+            return jsonify(response_object), 401
+    else:
+        return jsonify(response_object), 403
 
 @auth_blueprint.route('/auth/status', methods=['GET'])
 def status_user():  
