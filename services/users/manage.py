dfa400f2bd04d23d8593f4cc56044a7ea36f8fef
Nick Vandewiele <nickvandewiele@gmail.com>
2018-06-27 13:27:26 -0400

    add mock pw for users in seed_db function

--- services/users/manage.py
+++ services/users/manage.py
@@ -40,8 +40,8 @@ def test():
 @cli.command()
 def seed_db():
     """Seeds the database."""
-    db.session.add(User(username='michael', email="michael@realpython.com"))
-    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.add(User(username='michael', email="michael@realpython.com", password='greaterthaneight'))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org", password='greaterthaneight'))
     db.session.commit()
 
 
