be0b477f14474532802502a5dabc606619275a82
Nick Vandewiele <nickvandewiele@gmail.com>
2018-06-27 14:27:58 -0400

    /login route

--- services/users/project/api/auth.py
+++ services/users/project/api/auth.py
@@ -59,9 +59,39 @@ def register_user():
 
 
 @auth_blueprint.route('/auth/login', methods=['POST'])
-def user_login():   
-    pass
+def login_user():
+    
+    # get post data
+    post_data = request.get_json()
+    response_object = {
+        'status': 'fail',
+        'message': 'Invalid payload.'
+    }
+    if not post_data:
+        return jsonify(response_object), 400
 
+    email = post_data.get('email')
+    password = post_data.get('password')
+
+    try:
+        # fetch the user data
+        user = User.query.filter_by(email=email).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            auth_token = user.encode_auth_token(user.id)
+
+            if auth_token:
+                response_object['status'] = 'success'
+                response_object['message'] = 'Successfully logged in.'
+                response_object['auth_token'] = auth_token.decode()
+                return jsonify(response_object), 200
+            
+        else:
+            response_object['message'] = 'User does not exist.'
+            return jsonify(response_object), 404
+
+    except Exception as e:
+        response_object['message'] = 'Try again.'
+        return jsonify(response_object), 500
 
 @auth_blueprint.route('/auth/logout', methods=['GET'])
 def user_logout():  
