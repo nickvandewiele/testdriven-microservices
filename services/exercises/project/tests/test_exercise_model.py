20fa9295047fcf165cba7cd3fa79b7965e2fb0f6
Nick Vandewiele <nickvandewiele@gmail.com>
2018-07-31 19:26:00 -0400

    fix some linting issues in exercises service

--- services/exercises/project/tests/test_exercise_model.py
+++ services/exercises/project/tests/test_exercise_model.py
@@ -1,26 +1,24 @@
-from sqlalchemy.exc import IntegrityError
-
 from project import db
 from project.api.models import Exercise
 from project.tests.base import BaseTestCase
 
 
 class TestExerciseModel(BaseTestCase):
-	def test_add_exercise(self):
+    def test_add_exercise(self):
 
-		exercise = Exercise(
-			'Define a function called sum that takes two\
-			 integers as arguments and returns their sum.', 
-			'sum(2,2)', 
-			'4'
-		)
+        exercise = Exercise(
+            'Define a function called sum that takes two\
+             integers as arguments and returns their sum.',
+            'sum(2,2)',
+            '4'
+        )
 
-		db.session.add(exercise)
-		db.session.commit()
+        db.session.add(exercise)
+        db.session.commit()
 
-		self.assertTrue(exercise.id)
-		self.assertEquals(exercise.body, 'Define a function called sum that takes two\
-			 integers as arguments and returns their sum.')
+        self.assertTrue(exercise.id)
+        self.assertEquals(exercise.body, 'Define a function called sum that takes two\
+             integers as arguments and returns their sum.')
 
-		self.assertEquals(exercise.test_code, 'sum(2,2)')
-		self.assertEquals(exercise.test_code_solution, '4')
+        self.assertEquals(exercise.test_code, 'sum(2,2)')
+        self.assertEquals(exercise.test_code_solution, '4')
